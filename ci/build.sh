d1d4051f5c712d87fd426a0f219e99ce0349bc45
Vlad Alex <vlad.al.dp@gmail.com>
2019-09-17 15:34:52 +0300

    ironrdp: exclude ironrdp_client from CI wasm build

--- ci/build.sh
+++ ci/build.sh
@@ -6,8 +6,8 @@ cargo clippy --all-targets --all-features -- -D warnings
 cargo build
 cargo build --release
 
-cargo build --target wasm32-unknown-unknown
-cargo build --target wasm32-unknown-unknown --release
+cargo build --all --exclude=ironrdp_client --target wasm32-unknown-unknown
+cargo build --all --exclude=ironrdp_client --target wasm32-unknown-unknown --release
 
 cargo test
 cargo test --release
