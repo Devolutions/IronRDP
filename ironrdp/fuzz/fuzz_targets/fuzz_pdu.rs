62700600dba068677236a712fde092d741e5f97a
Sébastien Duquette <sduquette@devolutions.net>
2020-01-14 15:15:11 -0500

    Update the fuzzer

--- ironrdp/fuzz/fuzz_targets/fuzz_pdu.rs
+++ ironrdp/fuzz/fuzz_targets/fuzz_pdu.rs
@@ -3,27 +3,31 @@
 extern crate libfuzzer_sys;
 extern crate ironrdp;
 
-use ironrdp::gcc::*;
 use ironrdp::*;
-use ironrdp::rdp::server_license::*;
+use ironrdp::rdp::*;
 
 fuzz_target!(|data: &[u8]| {
     let _ = Request::from_buffer(data);
     let _ = Response::from_buffer(data);
     let _ = McsPdu::from_buffer(data);
-    let _ = ClientGccBlocks::from_buffer(data);
-    let _ = ServerGccBlocks::from_buffer(data);
-    let _ = ClientClusterData::from_buffer(data);
-    let _ = ConferenceCreateRequest::from_buffer(data);
-    let _ = ConferenceCreateResponse::from_buffer(data);
     let _ = ConnectInitial::from_buffer(data);
     let _ = ConnectResponse::from_buffer(data);
     let _ = ClientInfoPdu::from_buffer(data);
-    let _ = ClientNewLicenseRequest::from_buffer(data);
-    let _ = ClientPlatformChallengeResponse::from_buffer(data);
-    let _ = InitialServerLicenseMessage::from_buffer(data);
-    let _ = ServerLicenseRequest::from_buffer(data);
-    let _ = InitialServerLicenseMessage::from_buffer(data);
-    let _ = ServerPlatformChallenge::from_buffer(data);
     let _ = CapabilitySet::from_buffer(data);
+    let _ = ShareControlHeader::from_buffer(data);
+
+    let _ = gcc::ClientGccBlocks::from_buffer(data);
+    let _ = gcc::ServerGccBlocks::from_buffer(data);
+    let _ = gcc::ClientClusterData::from_buffer(data);
+    let _ = gcc::ConferenceCreateRequest::from_buffer(data);
+    let _ = gcc::ConferenceCreateResponse::from_buffer(data);
+
+    let _ = server_license::ClientNewLicenseRequest::from_buffer(data);
+    let _ = server_license::ClientPlatformChallengeResponse::from_buffer(data);
+    let _ = server_license::InitialServerLicenseMessage::from_buffer(data);
+    let _ = server_license::ServerLicenseRequest::from_buffer(data);
+    let _ = server_license::InitialServerLicenseMessage::from_buffer(data);
+    let _ = server_license::ServerPlatformChallenge::from_buffer(data);
+
+    let _ = vc::ChannelPduHeader::from_buffer(data);
 });
