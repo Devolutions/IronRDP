440b987a83c1cfbb433e1f4de45c17a1e438d108
Benoît CORTIER <bcortier@pm.me>
2022-04-14 17:04:16 -0400

    build: fix code using `bitvec` crate

--- ironrdp/src/utils.rs
+++ ironrdp/src/utils.rs
@@ -116,31 +116,32 @@ pub fn write_string_with_null_terminator(
 }
 
 pub struct Bits<'a> {
-    bits_slice: &'a BitSlice<Msb0, u8>,
+    bits_slice: &'a BitSlice<u8, Msb0>,
     remaining_bits_of_last_byte: usize,
 }
 
 impl<'a> Bits<'a> {
-    pub fn new(bits_slice: &'a BitSlice<Msb0, u8>) -> Self {
+    pub fn new(bits_slice: &'a BitSlice<u8, Msb0>) -> Self {
         Self {
             bits_slice,
             remaining_bits_of_last_byte: 0,
         }
     }
-    pub fn split_to(&mut self, at: usize) -> &'a BitSlice<Msb0, u8> {
+
+    pub fn split_to(&mut self, at: usize) -> &'a BitSlice<u8, Msb0> {
         let (value, new_bits) = self.bits_slice.split_at(at);
         self.bits_slice = new_bits;
         self.remaining_bits_of_last_byte = (self.remaining_bits_of_last_byte + at) % 8;
-
         value
     }
+
     pub fn remaining_bits_of_last_byte(&self) -> usize {
         self.remaining_bits_of_last_byte
     }
 }
 
 impl<'a> ops::Deref for Bits<'a> {
-    type Target = BitSlice<Msb0, u8>;
+    type Target = BitSlice<u8, Msb0>;
 
     fn deref(&self) -> &Self::Target {
         self.bits_slice
