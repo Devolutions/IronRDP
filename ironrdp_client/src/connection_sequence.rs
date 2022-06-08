1a968e543ca70b23d77ebdb2ff9590a4f32483f8
Alex <aleksandr.yusuk@apriorit.com>
2022-06-08 22:20:31 +0300

    credssp: replace Kerberos security package with Negotiate

--- ironrdp_client/src/connection_sequence.rs
+++ ironrdp_client/src/connection_sequence.rs
@@ -18,7 +18,7 @@ use ironrdp::{nego, rdp, PduParsing};
 use log::{debug, info, trace, warn};
 use ring::rand::SecureRandom;
 use sspi::internal::credssp;
-use sspi::KerberosConfig;
+use sspi::{KerberosConfig, NegotiateConfig};
 
 use crate::transport::*;
 use crate::{InputConfig, RdpError, BUF_STREAM_SIZE};
@@ -142,7 +142,8 @@ pub fn process_cred_ssp(
         server_public_key,
         credentials,
         credssp::CredSspMode::WithCredentials,
-        credssp::ClientMode::Kerberos(KerberosConfig::from_env()),
+        // credssp::ClientMode::Negotiate(NegotiateConfig::new(Some(KerberosConfig::from_env()))),
+        credssp::ClientMode::Negotiate(NegotiateConfig::new(None)),
         service_principal_name,
     )
     .map_err(RdpError::CredSspError)?;
