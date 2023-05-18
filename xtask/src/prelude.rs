339b1b62f37b62475f60547d01081ba00e369993
Benoît CORTIER <bcortier@proton.me>
2023-05-18 17:58:28 -0400

    ci: optimize speed and cache

--- xtask/src/prelude.rs
+++ xtask/src/prelude.rs
@@ -1,14 +1,11 @@
 pub use anyhow::Context as _;
 pub use xshell::{cmd, Shell};
 
+pub use crate::bin_version::*;
 pub use crate::section::Section;
 
 pub const CARGO: &str = env!("CARGO");
 pub const LOCAL_CARGO_ROOT: &str = ".cargo/local_root/";
-
-pub const CARGO_FUZZ_VERSION: &str = "0.11.2";
-pub const GRCOV_VERSION: &str = "0.8.18";
-pub const WASM_PACK_VERSION: &str = "0.11.1";
 
 pub const WASM_PACKAGES: &[&str] = &["ironrdp-web"];
 
