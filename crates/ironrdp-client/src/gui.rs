2398616c81e0f6990ed5ac4efba16f5640c3c73d
Benoît CORTIER <bcortier@proton.me>
2023-08-22 21:41:14 -0400

    fix: replace incorrect eprintln! usage by error!

--- crates/ironrdp-client/src/gui.rs
+++ crates/ironrdp-client/src/gui.rs
@@ -235,8 +235,8 @@ impl GuiContext {
                     window.set_cursor_visible(true);
                 }
                 Event::UserEvent(RdpOutputEvent::PointerPosition { x, y }) => {
-                    if let Err(e) = window.set_cursor_position(LogicalPosition::new(x as f64, y as f64)) {
-                        eprintln!("Failed to set cursor position: {}", e);
+                    if let Err(error) = window.set_cursor_position(LogicalPosition::new(x as f64, y as f64)) {
+                        error!(?error, "Failed to set cursor position");
                     }
                 }
                 Event::LoopDestroyed => {
