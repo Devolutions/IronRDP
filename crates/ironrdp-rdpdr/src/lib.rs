39caa0502370bcbb0dd0487add4acb9d3095849e
Isaiah Becker-Mayer <isaiah@goteleport.com>
2023-10-12 21:35:01 -0700

    ContextCall

--- crates/ironrdp-rdpdr/src/lib.rs
+++ crates/ironrdp-rdpdr/src/lib.rs
@@ -109,6 +109,7 @@ impl Rdpdr {
             let call = ScardCall::decode(req.io_control_code, payload)?;
 
             debug!(?req);
+            debug!(?req.io_control_code);
             debug!(?call);
 
             self.backend.handle_scard_call(req, call)?;
