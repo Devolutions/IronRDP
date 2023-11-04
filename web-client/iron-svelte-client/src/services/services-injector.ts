5b0094d40ff7b55fca3badf0aa7a2c1a4a2733bb
Benoît CORTIER <bcortier@proton.me>
2023-11-03 20:24:36 -0400

    refactor(svelte): remove dead code

--- web-client/iron-svelte-client/src/services/services-injector.ts
+++
@@ -1,8 +0,0 @@
-import type { ServerBridgeService } from './server-bridge.service';
-
-export let serverBridge: ServerBridgeService;
-
-import("./wasm-bridge.service").then(module => {
-    serverBridge = new module.WasmBridgeService();
-    serverBridge.init();
-});
