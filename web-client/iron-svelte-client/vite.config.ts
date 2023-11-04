dff3f307ebfd24692eca140c46baddb6e034e029
Benoît CORTIER <bcortier@proton.me>
2023-11-03 23:15:39 -0400

    build(web-client): update npm dependencies

--- web-client/iron-svelte-client/vite.config.ts
+++ web-client/iron-svelte-client/vite.config.ts
@@ -4,7 +4,7 @@ import wasm from 'vite-plugin-wasm';
 import topLevelAwait from 'vite-plugin-top-level-await';
 
 const config: UserConfig = {
-	mode: process.env.MODE || 'development',
+	mode: 'process.env.MODE' || 'development',
 	plugins: [sveltekit(), wasm(), topLevelAwait()],
 };
 
