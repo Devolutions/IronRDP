3725455682cc59e2fc3948be13c27b6f2690b2c3
Marc-Andre Lureau <marcandre.lureau@redhat.com>
2023-12-08 23:18:07 +0400

    feat(session): handle surface with codec ID 0 (raw bitmap) (#331)
    This fixes the compability with the server example.
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

--- crates/ironrdp-session/src/utils.rs
+++ crates/ironrdp-session/src/utils.rs
@@ -1,15 +1,16 @@
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 #[repr(u8)]
 pub enum CodecId {
+    None = 0x0,
     RemoteFx = 0x3,
 }
 
 impl CodecId {
     pub const fn from_u8(value: u8) -> Option<Self> {
-        if value == 0x3 {
-            Some(Self::RemoteFx)
-        } else {
-            None
+        match value {
+            0 => Some(Self::None),
+            3 => Some(Self::RemoteFx),
+            _ => None,
         }
     }
 }
