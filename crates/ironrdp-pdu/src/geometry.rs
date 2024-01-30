1548b580cee86e31025cc7a91562f6790501b430
Marc-André Lureau <marcandre.lureau@redhat.com>
2024-01-30 18:52:10 +0400

    refactor(pdu): remove dead code
    We may want to keep the TODO, although it's unclear to me. Perhaps
    better in a github issue instead.
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

--- crates/ironrdp-pdu/src/geometry.rs
+++ crates/ironrdp-pdu/src/geometry.rs
@@ -270,45 +270,6 @@ mod legacy {
 
     use super::*;
 
-    impl InclusiveRectangle {
-        // TODO: clarify code related to rectangles (inclusive vs exclusive bounds, …)
-        // See for instance:
-        // https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-rdpbcgr/84a3d4d2-5523-4e49-9a48-33952c559485
-        // https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-rdpbcgr/776dbdaf-7619-45fd-9a90-ebfd07802b24
-        // Rename / new structs "InclusiveRect" / "ExclusiveRect"…
-        // Also, avoid / audit mixed usage of "Rectangle" with "RfxRectangle"
-        // We should be careful when manipulating structs with slight nuances like this.
-
-        pub fn from_buffer_exclusive(mut stream: impl io::Read) -> Result<Self, io::Error> {
-            let left = stream.read_u16::<LittleEndian>()?;
-            let top = stream.read_u16::<LittleEndian>()?;
-            let right = stream
-                .read_u16::<LittleEndian>()?
-                .checked_sub(1)
-                .ok_or_else(|| io::Error::new(io::ErrorKind::Other, "invalid exclusive right bound"))?;
-            let bottom = stream
-                .read_u16::<LittleEndian>()?
-                .checked_sub(1)
-                .ok_or_else(|| io::Error::new(io::ErrorKind::Other, "invalid exclusive bottom bound"))?;
-
-            Ok(Self {
-                left,
-                top,
-                right,
-                bottom,
-            })
-        }
-
-        pub fn to_buffer_exclusive(&self, mut stream: impl io::Write) -> Result<(), io::Error> {
-            stream.write_u16::<LittleEndian>(self.left)?;
-            stream.write_u16::<LittleEndian>(self.top)?;
-            stream.write_u16::<LittleEndian>(self.right + 1)?;
-            stream.write_u16::<LittleEndian>(self.bottom + 1)?;
-
-            Ok(())
-        }
-    }
-
     impl crate::PduParsing for InclusiveRectangle {
         type Error = io::Error;
 
