282731bfe8317421120ec72b365d60df11a7284f
Marc-André Lureau <marcandre.lureau@redhat.com>
2024-02-13 18:19:17 +0400

    refactor(acceptor): drop legacy::encode_send_data_indication
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

--- crates/ironrdp-acceptor/src/util.rs
+++ crates/ironrdp-acceptor/src/util.rs
@@ -2,7 +2,7 @@ use std::borrow::Cow;
 
 use ironrdp_connector::{ConnectorError, ConnectorErrorExt, ConnectorResult};
 use ironrdp_pdu::write_buf::WriteBuf;
-use ironrdp_pdu::{encode_vec, rdp, PduEncode, PduParsing};
+use ironrdp_pdu::{encode_vec, rdp, PduEncode};
 
 pub(crate) fn encode_send_data_indication<T>(
     initiator_id: u16,
@@ -24,36 +24,6 @@ where
     let written = ironrdp_pdu::encode_buf(&pdu, buf).map_err(ConnectorError::pdu)?;
 
     Ok(written)
-}
-
-pub mod legacy {
-    use super::*;
-
-    pub(crate) fn encode_send_data_indication<T>(
-        initiator_id: u16,
-        channel_id: u16,
-        user_msg: &T,
-        buf: &mut WriteBuf,
-    ) -> ConnectorResult<usize>
-    where
-        T: PduParsing,
-        ConnectorError: From<T::Error>,
-    {
-        let user_data_len = user_msg.buffer_length();
-        let mut user_data = Vec::with_capacity(user_data_len);
-
-        user_msg.to_buffer(&mut user_data)?;
-
-        let pdu = ironrdp_pdu::mcs::SendDataIndication {
-            initiator_id,
-            channel_id,
-            user_data: Cow::Owned(user_data),
-        };
-
-        let written = ironrdp_pdu::encode_buf(&pdu, buf).map_err(ConnectorError::pdu)?;
-
-        Ok(written)
-    }
 }
 
 pub(crate) fn wrap_share_data(pdu: rdp::headers::ShareDataPdu, io_channel_id: u16) -> rdp::headers::ShareControlHeader {
