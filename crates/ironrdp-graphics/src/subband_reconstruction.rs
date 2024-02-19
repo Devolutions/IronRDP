48aff67d9909f9cf0b2d8aa5e618b9673c43be95
Marc-André Lureau <marcandre.lureau@redhat.com>
2024-02-19 23:36:47 +0400

    feat(graphics): add rfx-encoding subband
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

--- crates/ironrdp-graphics/src/subband_reconstruction.rs
+++ crates/ironrdp-graphics/src/subband_reconstruction.rs
@@ -4,6 +4,18 @@ pub fn decode(buffer: &mut [i16]) {
     }
 }
 
+pub fn encode(buffer: &mut [i16]) {
+    if buffer.is_empty() {
+        return;
+    }
+    let mut prev = buffer[0];
+    for buf in buffer.iter_mut().skip(1) {
+        let b = *buf;
+        *buf = b.overflowing_sub(prev).0;
+        prev = b;
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -39,10 +51,26 @@ mod tests {
     }
 
     #[test]
+    fn encode_changes_last_element_for_buffer_with_min_elements() {
+        let mut buffer = [-32768, 0, -32768, 0, -32768];
+        let expected = [-32768, -32768, -32768, -32768, -32768];
+        encode(&mut buffer);
+        assert_eq!(expected, buffer);
+    }
+
+    #[test]
     fn decode_changes_last_element_for_buffer_with_max_elements() {
         let mut buffer = [32767, 32767, 32767, 32767, 32767];
         let expected = [32767, -2, 32765, -4, 32763];
         decode(&mut buffer);
+        assert_eq!(expected, buffer);
+    }
+
+    #[test]
+    fn encode_changes_last_element_for_buffer_with_max_elements() {
+        let mut buffer = [32767, -2, 32765, -4, 32763];
+        let expected = [32767, 32767, 32767, 32767, 32767];
+        encode(&mut buffer);
         assert_eq!(expected, buffer);
     }
 
@@ -55,10 +83,26 @@ mod tests {
     }
 
     #[test]
+    fn encode_does_not_change_zeroed_buffer() {
+        let mut buffer = [0, 0, 0, 0, 0];
+        let expected = [0, 0, 0, 0, 0];
+        encode(&mut buffer);
+        assert_eq!(expected, buffer);
+    }
+
+    #[test]
     fn decode_works_with_five_not_zeroed_elements() {
         let mut buffer = [1, 2, 3, 4, 5];
         let expected = [1, 3, 6, 10, 15];
         decode(&mut buffer);
+        assert_eq!(expected, buffer);
+    }
+
+    #[test]
+    fn encode_works_with_five_not_zeroed_elements() {
+        let mut buffer = [1, 3, 6, 10, 15];
+        let expected = [1, 2, 3, 4, 5];
+        encode(&mut buffer);
         assert_eq!(expected, buffer);
     }
 }
