94c176bf6a9bf0288c5943a607f1f8c6e35fc18c
Isaiah Becker-Mayer <isaiah@goteleport.com>
2024-03-25 19:58:49 -0500

    Adds a callback mechanism to the DisplayControlClient which is called when the server sends the DisplayControlCapabilities (indicating that the client is ready for use

--- crates/ironrdp-displaycontrol/src/client.rs
+++ crates/ironrdp-displaycontrol/src/client.rs
@@ -1,14 +1,51 @@
 use crate::{
-    pdu::{DisplayControlMonitorLayout, DisplayControlPdu, MonitorLayoutEntry},
+    pdu::{DisplayControlCapabilities, DisplayControlPdu},
     CHANNEL_NAME,
 };
 use ironrdp_dvc::{encode_dvc_messages, DvcClientProcessor, DvcMessage, DvcProcessor};
-use ironrdp_pdu::PduResult;
+use ironrdp_pdu::{cursor::ReadCursor, PduDecode, PduResult};
 use ironrdp_svc::{impl_as_any, ChannelFlags, SvcMessage};
 use tracing::debug;
 
 /// A client for the Display Control Virtual Channel.
-pub struct DisplayControlClient {}
+pub struct DisplayControlClient {
+    /// A callback that will be called when capabilities are received from the server.
+    /// If no callback is set, a default (inert) callback will be used.
+    on_capabilities_received: OnCapabilitiesReceived,
+}
+
+impl Default for DisplayControlClient {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl DisplayControlClient {
+    pub fn new() -> Self {
+        Self {
+            on_capabilities_received: Box::new(|_| {
+                debug!("No capabilities received callback set, ignoring.");
+                Ok(Vec::new())
+            }),
+        }
+    }
+
+    /// Sets a callback that will be called when capabilities are received from the server.
+    #[must_use]
+    pub fn with_capabilities_received_callback<F>(mut self, callback: F) -> Self
+    where
+        F: Fn(DisplayControlCapabilities) -> PduResult<Vec<DvcMessage>> + Send + Sync + 'static,
+    {
+        self.on_capabilities_received = Box::new(callback);
+        self
+    }
+
+    /// Builds a [`DisplayControlPdu::MonitorLayout`] as an [`SvcMessage`] for a monitor with the given dimensions.
+    pub fn encode_monitor(&self, channel_id: u32, width: u32, height: u32) -> PduResult<Vec<SvcMessage>> {
+        let pdu = DisplayControlPdu::create_monitor_layout_pdu(width, height)?;
+        encode_dvc_messages(channel_id, vec![Box::new(pdu)], ChannelFlags::empty())
+    }
+}
 
 impl_as_any!(DisplayControlClient);
 
@@ -22,29 +59,12 @@ impl DvcProcessor for DisplayControlClient {
     }
 
     fn process(&mut self, _channel_id: u32, payload: &[u8]) -> PduResult<Vec<DvcMessage>> {
-        // TODO: We can parse the payload here for completeness sake,
-        // in practice we don't need to do anything with the payload.
-        debug!("Got Display PDU of length: {}", payload.len());
-        Ok(Vec::new())
+        let caps = DisplayControlCapabilities::decode(&mut ReadCursor::new(payload))?;
+        debug!("received {:?}", caps);
+        (self.on_capabilities_received)(caps)
     }
 }
 
 impl DvcClientProcessor for DisplayControlClient {}
 
-impl DisplayControlClient {
-    pub fn new() -> Self {
-        Self {}
-    }
-
-    /// Fully encodes a [`MonitorLayoutPdu`] with the given monitors.
-    pub fn encode_monitors(&self, channel_id: u32, monitors: Vec<MonitorLayoutEntry>) -> PduResult<Vec<SvcMessage>> {
-        let pdu: DisplayControlPdu = DisplayControlMonitorLayout::new(&monitors)?.into();
-        encode_dvc_messages(channel_id, vec![Box::new(pdu)], ChannelFlags::empty())
-    }
-}
-
-impl Default for DisplayControlClient {
-    fn default() -> Self {
-        Self::new()
-    }
-}
+type OnCapabilitiesReceived = Box<dyn Fn(DisplayControlCapabilities) -> PduResult<Vec<DvcMessage>> + Send + Sync>;
--- crates/ironrdp-displaycontrol/src/pdu/mod.rs
+++ crates/ironrdp-displaycontrol/src/pdu/mod.rs
@@ -30,6 +30,18 @@ pub enum DisplayControlPdu {
 impl DisplayControlPdu {
     const NAME: &'static str = "DISPLAYCONTROL_HEADER";
     const FIXED_PART_SIZE: usize = 4 /* Type */ + 4 /* Length */;
+
+    /// Creates a new [`DisplayControlPdu::MonitorLayout`] PDU for a single monitor with the given width and height.
+    pub fn create_monitor_layout_pdu(width: u32, height: u32) -> PduResult<Self> {
+        let monitors = vec![
+            MonitorLayoutEntry::new_primary(width, height)?.with_orientation(if width > height {
+                MonitorOrientation::Landscape
+            } else {
+                MonitorOrientation::Portrait
+            }),
+        ];
+        Ok(DisplayControlMonitorLayout::new(&monitors).unwrap().into())
+    }
 }
 
 impl PduEncode for DisplayControlPdu {
