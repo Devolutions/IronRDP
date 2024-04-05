e833dcec5e9179fb61c8457faa42106b4d7fe164
irving ou <jou@devolutions.net>
2024-04-05 16:46:41 -0400

    WIP:add byte list

--- ffi/dotnet/Devolutions.IronRdp/src/ByteList.cs
+++ ffi/dotnet/Devolutions.IronRdp/src/ByteList.cs
@@ -1,57 +1,55 @@
-
 public class ByteList {
 
-    private inner: byte[]
-    private occupied: int
+    private byte[] inner;
+    private int occupied = 0;
+    private int capacity;
 
     public ByteList(int capacity) {
-        this.capacity = capacity
-        this.inner = new byte[capacity]
+        this.capacity = capacity;
+        this.inner = new byte[capacity];
     }
 
     public ByteList(int capacity, byte[] bytes) {
-        this.capacity = capacity
-        this.inner = new byte[capacity]
-        this.AddRange(bytes)
+        this.capacity = capacity;
+        this.inner = new byte[capacity];
+        this.AddRange(bytes);
     }
 
     public byte[] Peek() {
-        return this.inner
+        return java.util.Arrays.copyOf(inner, occupied);
     }
 
     public void AddRange(byte[] bytes) {
-        while (bytes.Length > this.capacity) {
-            this.Grow()
+        while (bytes.length + occupied > capacity) {
+            this.Grow();
         }
-        Array.Copy(bytes, 0, this.inner, 0, bytes.Length)
+        System.arraycopy(bytes, 0, this.inner, occupied, bytes.length);
+        occupied += bytes.length;
     }
 
-    /// <summary>
-    /// Split the ByteList into two parts. The first part will returned and removed from the ByteList. The second part will remain in the ByteList.
-    /// The Returned Part will be contain [0, at) bytes from the ByteList. and the ByteList will contain [at, occupied) bytes.
-    /// </summary>
-    /// <param name="at">The size of the byte array to split.</param>
-    /// <returns>A new byte array containing the split bytes.</returns>
     public ByteList SplitTo(int at) {
-        if (at > this.occupied) {
-            throw new Exception("Not enough bytes to split")
+        if (at > occupied) {
+            throw new IllegalArgumentException("Not enough bytes to split");
         }
 
-        var res = new byte[at]
-        Array.Copy(this.inner, 0, res, 0, at)
-        Array.Copy(this.inner, at, this.inner, 0, this.occupied - at)
-        this.occupied -= at
-        return ByteList(at, res)
+        byte[] res = new byte[at];
+        System.arraycopy(this.inner, 0, res, 0, at);
+        System.arraycopy(this.inner, at, this.inner, 0, occupied - at);
+        occupied -= at;
+
+        ByteList resultByteList = new ByteList(capacity, res);
+        return resultByteList;
     }
 
     private void Grow() {
-        if (this.capacity == 0) {
-            this.capacity = 1
+        if (capacity == 0) {
+            capacity = 1;
         } else {
-            this.capacity *= 2
+            capacity *= 2;
         }
 
-        var newInner = new byte[this.capacity]
-        Array.Copy(this.inner, 0, newInner, 0, this.occupied)
+        byte[] newInner = new byte[capacity];
+        System.arraycopy(inner, 0, newInner, 0, occupied);
+        inner = newInner;
     }
 }
