ab2a8a38e121ec487e2b4db830678cc0620b455a
Isaiah Becker-Mayer <isaiah@goteleport.com>
2024-04-26 16:20:48 -0500

    Update crates/ironrdp-connector/src/connection_activation.rs
    Co-authored-by: Benoît Cortier <bcortier@proton.me>

--- crates/ironrdp-connector/src/connection_activation.rs
+++ crates/ironrdp-connector/src/connection_activation.rs
@@ -365,7 +365,7 @@ fn create_client_confirm_active(
             })),
         }])),
         CapabilitySet::FrameAcknowledge(FrameAcknowledge {
-            // TODO: Revert this to 2 per FreeRDP.
+            // FIXME(#447): Revert this to 2 per FreeRDP.
             // This is a temporary hack to fix a resize bug, see:
             // https://github.com/Devolutions/IronRDP/issues/447
             max_unacknowledged_frame_count: 20,
