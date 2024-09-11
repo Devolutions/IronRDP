c01c3828545ffd6e87d1a27c19bc8fb0a4698bfc
Marc-André Lureau <marcandre.lureau@redhat.com>
2024-09-11 17:16:31 +0400

    feat(server): warn if encoding takes >10ms
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

--- crates/ironrdp-server/src/lib.rs
+++ crates/ironrdp-server/src/lib.rs
@@ -28,3 +28,27 @@ pub use handler::*;
 pub use helper::*;
 pub use server::*;
 pub use sound::*;
+
+#[macro_export]
+macro_rules! time_warn {
+    ($context:expr, $threshold_ms:expr, $op:expr) => {{
+        #[cold]
+        fn warn_log(context: &str, duration: u128) {
+            use std::sync::atomic::AtomicUsize;
+
+            static COUNT: AtomicUsize = AtomicUsize::new(0);
+            let current_count = COUNT.fetch_add(1, ::std::sync::atomic::Ordering::Relaxed);
+            if current_count < 50 || current_count % 100 == 0 {
+                ::tracing::warn!("{context} took {duration} ms! (count: {current_count})");
+            }
+        }
+
+        let start = std::time::Instant::now();
+        let result = $op;
+        let duration = start.elapsed().as_millis();
+        if duration > $threshold_ms {
+            warn_log($context, duration);
+        }
+        result
+    }};
+}
