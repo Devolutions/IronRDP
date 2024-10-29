5381b2444416535d3f21c070ced079de1988f18d
Marc-André Lureau <marcandre.lureau@redhat.com>
2024-10-29 12:36:18 +0400

    docs: use Devolutions logo
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

--- crates/ironrdp-cliprdr-native/src/lib.rs
+++ crates/ironrdp-cliprdr-native/src/lib.rs
@@ -1,4 +1,7 @@
 #![doc = include_str!("../README.md")]
+#![doc(
+    html_logo_url = "https://webdevolutions.blob.core.windows.net/images/projects/devolutions/logos/devolutions-icon-shadow.svg"
+)]
 #![warn(unsafe_op_in_unsafe_fn)]
 #![warn(invalid_reference_casting)]
 #![warn(clippy::undocumented_unsafe_blocks)]
--- crates/ironrdp/src/lib.rs
+++ crates/ironrdp/src/lib.rs
@@ -1,4 +1,7 @@
 #![doc = include_str!("../../../README.md")]
+#![doc(
+    html_logo_url = "https://webdevolutions.blob.core.windows.net/images/projects/devolutions/logos/devolutions-icon-shadow.svg"
+)]
 
 #[cfg(feature = "acceptor")]
 pub use ironrdp_acceptor as acceptor;
