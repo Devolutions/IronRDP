b279b96e7dcf7ea0338ae946f57bfc3fc1db77de
Benoît CORTIER <bcortier@proton.me>
2024-11-20 20:06:04 +0900

    docs(ironrdp): README.md dedicated to the meta crate
    Using the top-level README.md of the repository is not ideal.
    
    1/ It contains many information unrelated to the meta crate itself.
    
    2/ The following attribute:
    
    ```
     #![doc = include_str!("../../../README.md")]
    ```
    
    will fail at finding the README.md when built via `cargo package`.

---
+++ crates/ironrdp/README.md
@@ -0,0 +1,7 @@
+# IronRDP meta crate
+
+A meta crate re-exporting IronRDP crates for convenience.
+
+This crate is part of the [IronRDP] project.
+
+[IronRDP]: https://github.com/Devolutions/IronRDP
--- crates/ironrdp/src/lib.rs
+++ crates/ironrdp/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc = include_str!("../../../README.md")]
+#![doc = include_str!("../README.md")]
 #![doc(
     html_logo_url = "https://webdevolutions.blob.core.windows.net/images/projects/devolutions/logos/devolutions-icon-shadow.svg"
 )]
