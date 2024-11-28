0ee5bfc5614c4a1b7a5b1da59da9f2eeaca2aded
Marc-André Lureau <marcandre.lureau@redhat.com>
2024-11-28 17:52:33 +0400

    fix(examples): fix screenshot deps
    Set the required-features and use sspi with "network_client".
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

--- crates/ironrdp/src/lib.rs
+++ crates/ironrdp/src/lib.rs
@@ -6,7 +6,7 @@
 #[cfg(test)]
 use {
     anyhow as _, async_trait as _, bmp as _, ironrdp_blocking as _, ironrdp_cliprdr_native as _, pico_args as _,
-    rand as _, tokio_rustls as _, tracing as _, tracing_subscriber as _, x509_cert as _,
+    rand as _, sspi as _, tokio_rustls as _, tracing as _, tracing_subscriber as _, x509_cert as _,
 };
 
 #[cfg(feature = "acceptor")]
