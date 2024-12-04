d83b82b3a494f66362fa9313e603a3a468c195a4
Vladyslav Nikonov <mail@pacmancoder.xyz>
2024-12-04 18:06:25 +0200

    docs(now-proto): add rendered PDF for now-proto 1.0

---
+++ docs/make.ps1
@@ -0,0 +1,3 @@
+# NOTE: Markdown Monster CLI may be broken at the moment (WinGet version 3.5.4 definitely is),
+# the PDF could be generated manually via GUI instead.
+mmcli markdowntopdf -i NOW-spec.md -o NOW-spec.pdf --theme Github --orientation Portrait --page-size A4
