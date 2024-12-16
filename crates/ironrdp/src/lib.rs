f88185d9af8422fe40480a6183c9f65333bc53de
Benoît CORTIER <git.divisible626@passmail.com>
2024-12-16 17:10:18 +0900

    docs(ironrdp): inline documentation for re-exported items

--- crates/ironrdp/src/lib.rs
+++ crates/ironrdp/src/lib.rs
@@ -10,30 +10,57 @@ use {
 };
 
 #[cfg(feature = "acceptor")]
+#[doc(inline)]
 pub use ironrdp_acceptor as acceptor;
+
 #[cfg(feature = "cliprdr")]
+#[doc(inline)]
 pub use ironrdp_cliprdr as cliprdr;
+
 #[cfg(feature = "connector")]
+#[doc(inline)]
 pub use ironrdp_connector as connector;
+
 #[cfg(feature = "core")]
+#[doc(inline)]
 pub use ironrdp_core as core;
+
 #[cfg(feature = "displaycontrol")]
+#[doc(inline)]
 pub use ironrdp_displaycontrol as displaycontrol;
+
 #[cfg(feature = "dvc")]
+#[doc(inline)]
 pub use ironrdp_dvc as dvc;
+
 #[cfg(feature = "graphics")]
+#[doc(inline)]
 pub use ironrdp_graphics as graphics;
+
 #[cfg(feature = "input")]
+#[doc(inline)]
 pub use ironrdp_input as input;
+
 #[cfg(feature = "pdu")]
+#[doc(inline)]
 pub use ironrdp_pdu as pdu;
+
 #[cfg(feature = "rdpdr")]
+#[doc(inline)]
 pub use ironrdp_rdpdr as rdpdr;
+
 #[cfg(feature = "rdpsnd")]
+#[doc(inline)]
 pub use ironrdp_rdpsnd as rdpsnd;
+
 #[cfg(feature = "server")]
+#[doc(inline)]
 pub use ironrdp_server as server;
+
 #[cfg(feature = "session")]
+#[doc(inline)]
 pub use ironrdp_session as session;
+
 #[cfg(feature = "svc")]
+#[doc(inline)]
 pub use ironrdp_svc as svc;
