46e4c6785e64d56aacd6fb7c1a05fa9f61e008e7
Marc-André Lureau <marcandre.lureau@redhat.com>
2025-03-18 13:48:06 +0400

    feat(session): make client_codecs_capabilities() configurable
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

--- crates/ironrdp-session/src/utils.rs
+++
@@ -1,4 +0,0 @@
-use ironrdp_pdu::rdp::capability_sets::{
-    BitmapCodecs, CaptureFlags, Codec, CodecProperty, EntropyBits, RemoteFxContainer, RfxCaps, RfxCapset,
-    RfxClientCapsContainer, RfxICap, RfxICapFlags,
-};
