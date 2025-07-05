30ddbf42e398d21b43e1d8f53784195e26c77a80
irving ou <jou@devolutions.net>
2025-07-04 21:20:10 -0400

    feat(ffi): expose API for RDCleanPath

--- ffi/src/utils/mod.rs
+++ ffi/src/utils/mod.rs
@@ -4,6 +4,29 @@ pub mod ffi {
     use crate::error::ffi::IronRdpError;
 
     #[diplomat::opaque]
+    pub struct VecVecU8(pub Vec<Vec<u8>>);
+
+    impl VecVecU8 {
+        pub fn get_len(&self) -> usize {
+            self.0.len()
+        }
+
+        pub fn get_vecu8(&self, index: usize) -> Result<Box<VecU8>, Box<IronRdpError>> {
+            if index >= self.0.len() {
+                return Err("index out of bounds".into());
+            }
+            Ok(Box::new(VecU8(self.0[index].clone())))
+        }
+
+        pub fn get_slice<'a>(&'a self, index: usize) -> Result<Box<BytesSlice<'a>>, Box<IronRdpError>> {
+            if index >= self.0.len() {
+                return Err("index out of bounds".into());
+            }
+            Ok(Box::new(BytesSlice(&self.0[index])))
+        }
+    }
+
+    #[diplomat::opaque]
     pub struct VecU8(pub Vec<u8>);
 
     impl VecU8 {
@@ -78,5 +101,28 @@ pub mod ffi {
         pub fn get(&self) -> Result<usize, Box<IronRdpError>> {
             self.0.ok_or_else(|| "value is None".into())
         }
+    }
+
+    #[diplomat::opaque]
+    pub struct OptionalString(pub(crate) Option<String>);
+
+    impl OptionalString {
+        pub fn is_some(&self) -> bool {
+            self.0.is_some()
+        }
+
+        pub fn new(value: &str) -> Box<OptionalString> {
+            Box::new(OptionalString(Some(value.to_owned())))
+        }
+
+        pub fn new_empty() -> Box<OptionalString> {
+            Box::new(OptionalString(None))
+        }
+    }
+}
+
+impl From<&ffi::OptionalString> for Option<String> {
+    fn from(value: &ffi::OptionalString) -> Self {
+        value.0.clone()
     }
 }
