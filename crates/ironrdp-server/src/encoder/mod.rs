Devolutions/IronRDP

commit 87df67fdc76ff4f39d4b83521e34bf3b5e2e73bb

feat: add QOIZ image codec
Add a new QOIZ codec (UUID 229cc6dc-a860-4b52-b4d8-053a22b3892b) for
SetSurface command. The PDU data contains the same data as the QOI
codec, with zstd compression.

Some benchmarks showing interesting results (using ironrdp/perfenc)

QOI: 10s user CPU, 96.20% compression
QOIZ: 11s user CPU, 99.76% compression

Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

crates/ironrdp-server/src/encoder/mod.rs
@@ -1,7 +1,8 @@
 use core::fmt;
 use core::num::NonZeroU16;
+use std::sync::{Arc, Mutex};
 
-use anyhow::{Context, Result};
+use anyhow::{anyhow, Context, Result};
 use ironrdp_acceptor::DesktopSize;
 use ironrdp_graphics::diff::{find_different_rects_sub, Rect};
 use ironrdp_pdu::encode_vec;
@@ -35,6 +36,8 @@ pub(crate) struct UpdateEncoderCodecs {
     remotefx: Option<(EntropyBits, u8)>,
     #[cfg(feature = "qoi")]
     qoi: Option<u8>,
+    #[cfg(feature = "qoiz")]
+    qoiz: Option<u8>,
 }
 
 impl UpdateEncoderCodecs {
@@ -44,6 +47,8 @@ impl UpdateEncoderCodecs {
             remotefx: None,
             #[cfg(feature = "qoi")]
             qoi: None,
+            #[cfg(feature = "qoiz")]
+            qoiz: None,
         }
     }
 
@@ -56,6 +61,12 @@ impl UpdateEncoderCodecs {
     #[cfg_attr(feature = "__bench", visibility::make(pub))]
     pub(crate) fn set_qoi(&mut self, qoi: Option<u8>) {
         self.qoi = qoi
+    }
+
+    #[cfg(feature = "qoiz")]
+    #[cfg_attr(feature = "__bench", visibility::make(pub))]
+    pub(crate) fn set_qoiz(&mut self, qoiz: Option<u8>) {
+        self.qoiz = qoiz
     }
 }
 
@@ -93,6 +104,10 @@ impl UpdateEncoder {
             #[cfg(feature = "qoi")]
             if let Some(id) = codecs.qoi {
                 bitmap = BitmapUpdater::Qoi(QoiHandler::new(id));
+            }
+            #[cfg(feature = "qoiz")]
+            if let Some(id) = codecs.qoiz {
+                bitmap = BitmapUpdater::Qoiz(QoizHandler::new(id));
             }
 
             bitmap
@@ -306,6 +321,8 @@ enum BitmapUpdater {
     RemoteFx(RemoteFxHandler),
     #[cfg(feature = "qoi")]
     Qoi(QoiHandler),
+    #[cfg(feature = "qoiz")]
+    Qoiz(QoizHandler),
 }
 
 impl BitmapUpdater {
@@ -316,6 +333,8 @@ impl BitmapUpdater {
             Self::RemoteFx(up) => up.handle(bitmap),
             #[cfg(feature = "qoi")]
             Self::Qoi(up) => up.handle(bitmap),
+            #[cfg(feature = "qoiz")]
+            Self::Qoiz(up) => up.handle(bitmap),
         }
     }
 
@@ -445,26 +464,83 @@ impl QoiHandler {
 #[cfg(feature = "qoi")]
 impl BitmapUpdateHandler for QoiHandler {
     fn handle(&mut self, bitmap: &BitmapUpdate) -> Result<UpdateFragmenter> {
-        use ironrdp_graphics::image_processing::PixelFormat::*;
-
-        let raw_channels = match bitmap.format {
-            ARgb32 => qoi::RawChannels::Argb,
-            XRgb32 => qoi::RawChannels::Xrgb,
-            ABgr32 => qoi::RawChannels::Abgr,
-            XBgr32 => qoi::RawChannels::Xbgr,
-            BgrA32 => qoi::RawChannels::Bgra,
-            BgrX32 => qoi::RawChannels::Bgrx,
-            RgbA32 => qoi::RawChannels::Rgba,
-            RgbX32 => qoi::RawChannels::Rgbx,
-        };
-
-        let enc = qoi::EncoderBuilder::new(&bitmap.data, bitmap.width.get().into(), bitmap.height.get().into())
-            .stride(bitmap.stride)
-            .raw_channels(raw_channels)
-            .build()?;
-        let data = enc.encode_to_vec()?;
+        let data = qoi_encode(bitmap)?;
         set_surface(bitmap, self.codec_id, &data)
     }
+}
+
+#[cfg(feature = "qoiz")]
+#[derive(Clone)]
+struct QoizHandler {
+    codec_id: u8,
+    zctxt: Arc<Mutex<zstd_safe::CCtx<'static>>>,
+}
+
+#[cfg(feature = "qoiz")]
+impl fmt::Debug for QoizHandler {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("QoizHandler").field("codec_id", &self.codec_id).finish()
+    }
+}
+
+#[cfg(feature = "qoiz")]
+impl QoizHandler {
+    fn new(codec_id: u8) -> Self {
+        let mut zctxt = zstd_safe::CCtx::default();
+
+        zctxt.set_parameter(zstd_safe::CParameter::CompressionLevel(3)).unwrap();
+        zctxt
+            .set_parameter(zstd_safe::CParameter::EnableLongDistanceMatching(true))
+            .unwrap();
+        let zctxt = Arc::new(Mutex::new(zctxt));
+
+        Self { codec_id, zctxt }
+    }
+}
+
+#[cfg(feature = "qoiz")]
+impl BitmapUpdateHandler for QoizHandler {
+    fn handle(&mut self, bitmap: &BitmapUpdate) -> Result<UpdateFragmenter> {
+        let qoi = qoi_encode(bitmap)?;
+        let mut inb = zstd_safe::InBuffer::around(&qoi);
+        let mut data = vec![0; qoi.len()];
+        let mut outb = zstd_safe::OutBuffer::around(data.as_mut_slice());
+
+        let mut zctxt = self.zctxt.lock().unwrap();
+        let res = zctxt
+            .compress_stream2(
+                &mut outb,
+                &mut inb,
+                zstd_safe::zstd_sys::ZSTD_EndDirective::ZSTD_e_flush,
+            )
+            .map_err(zstd_safe::get_error_name)
+            .unwrap();
+        if res != 0 {
+            return Err(anyhow!("Failed to zstd compress"));
+        }
+
+        set_surface(bitmap, self.codec_id, outb.as_slice())
+    }
+}
+
+#[cfg(feature = "qoi")]
+fn qoi_encode(bitmap: &BitmapUpdate) -> Result<Vec<u8>> {
+    use ironrdp_graphics::image_processing::PixelFormat::*;
+    let raw_channels = match bitmap.format {
+        ARgb32 => qoi::RawChannels::Argb,
+        XRgb32 => qoi::RawChannels::Xrgb,
+        ABgr32 => qoi::RawChannels::Abgr,
+        XBgr32 => qoi::RawChannels::Xbgr,
+        BgrA32 => qoi::RawChannels::Bgra,
+        BgrX32 => qoi::RawChannels::Bgrx,
+        RgbA32 => qoi::RawChannels::Rgba,
+        RgbX32 => qoi::RawChannels::Rgbx,
+    };
+    let enc = qoi::EncoderBuilder::new(&bitmap.data, bitmap.width.get().into(), bitmap.height.get().into())
+        .stride(bitmap.stride)
+        .raw_channels(raw_channels)
+        .build()?;
+    Ok(enc.encode_to_vec()?)
 }
 
 fn set_surface(bitmap: &BitmapUpdate, codec_id: u8, data: &[u8]) -> Result<UpdateFragmenter> {

benches/Cargo.toml
@@ -10,8 +10,9 @@ name = "perfenc"
 path = "src/perfenc.rs"
 
 [features]
-default = ["qoi"]
+default = ["qoi", "qoiz"]
 qoi = ["ironrdp/qoi"]
+qoiz = ["ironrdp/qoiz"]
 
 [dependencies]
 anyhow = "1.0.98"

benches/src/perfenc.rs
@@ -28,7 +28,7 @@ async fn main() -> Result<(), anyhow::Error> {
         println!("  --width <WIDTH>      Width of the display (default: 3840)");
         println!("  --height <HEIGHT>    Height of the display (default: 2400)");
         println!("  --codec <CODEC>      Codec to use (default: remotefx)");
-        println!("                        Valid values: qoi, remotefx, bitmap, none");
+        println!("                        Valid values: qoi, qoiz, remotefx, bitmap, none");
         println!("  --fps <FPS>          Frames per second (default: none)");
         std::process::exit(0);
     }
@@ -54,6 +54,8 @@ async fn main() -> Result<(), anyhow::Error> {
         OptCodec::None => {}
         #[cfg(feature = "qoi")]
         OptCodec::Qoi => update_codecs.set_qoi(Some(0)),
+        #[cfg(feature = "qoiz")]
+        OptCodec::QoiZ => update_codecs.set_qoiz(Some(0)),
     };
 
     let mut encoder = UpdateEncoder::new(DesktopSize { width, height }, flags, update_codecs);
@@ -176,6 +178,8 @@ enum OptCodec {
     None,
     #[cfg(feature = "qoi")]
     Qoi,
+    #[cfg(feature = "qoiz")]
+    QoiZ,
 }
 
 impl Default for OptCodec {
@@ -194,6 +198,8 @@ impl core::str::FromStr for OptCodec {
             "none" => Ok(Self::None),
             #[cfg(feature = "qoi")]
             "qoi" => Ok(Self::Qoi),
+            #[cfg(feature = "qoiz")]
+            "qoiz" => Ok(Self::QoiZ),
             _ => Err(anyhow::anyhow!("unknown codec: {}", s)),
         }
     }

crates/ironrdp-client/Cargo.toml
@@ -28,6 +28,7 @@ default = ["rustls"]
 rustls = ["ironrdp-tls/rustls", "tokio-tungstenite/rustls-tls-native-roots"]
 native-tls = ["ironrdp-tls/native-tls", "tokio-tungstenite/native-tls"]
 qoi = ["ironrdp/qoi"]
+qoiz = ["ironrdp/qoiz"]
 
 [dependencies]
 # Protocols

crates/ironrdp-connector/Cargo.toml
@@ -19,6 +19,7 @@ test = false
 default = []
 arbitrary = ["dep:arbitrary"]
 qoi = ["ironrdp-pdu/qoi"]
+qoiz = ["ironrdp-pdu/qoiz"]
 
 [dependencies]
 ironrdp-svc = { path = "../ironrdp-svc", version = "0.4" } # public

crates/ironrdp-pdu/Cargo.toml
@@ -20,6 +20,7 @@ default = []
 std = ["alloc", "ironrdp-error/std", "ironrdp-core/std"]
 alloc = ["ironrdp-core/alloc", "ironrdp-error/alloc"]
 qoi = []
+qoiz = ["qoi"]
 
 [dependencies]
 bitflags = "2.9"

crates/ironrdp-pdu/src/rdp/capability_sets.rs
@@ -34,7 +34,7 @@ pub use self::bitmap_cache::{
 pub use self::bitmap_codecs::{
     client_codecs_capabilities, server_codecs_capabilities, BitmapCodecs, CaptureFlags, Codec, CodecId, CodecProperty,
     EntropyBits, Guid, NsCodec, RemoteFxContainer, RfxCaps, RfxCapset, RfxClientCapsContainer, RfxICap, RfxICapFlags,
-    CODEC_ID_NONE, CODEC_ID_QOI, CODEC_ID_REMOTEFX,
+    CODEC_ID_NONE, CODEC_ID_QOI, CODEC_ID_QOIZ, CODEC_ID_REMOTEFX,
 };
 pub use self::brush::{Brush, SupportLevel};
 pub use self::frame_acknowledge::FrameAcknowledge;

crates/ironrdp-pdu/src/rdp/capability_sets/bitmap_codecs.rs
@@ -43,6 +43,9 @@ const GUID_IGNORE: Guid = Guid(0x9c43_51a6, 0x3535, 0x42ae, 0x91, 0x0c, 0xcd, 0x
 #[rustfmt::skip]
 #[cfg(feature="qoi")]
 const GUID_QOI: Guid = Guid(0x4dae_9af8, 0xb399, 0x4df6, 0xb4, 0x3a, 0x66, 0x2f, 0xd9, 0xc0, 0xf5, 0xd6);
+#[rustfmt::skip]
+#[cfg(feature="qoiz")]
+const GUID_QOIZ: Guid = Guid(0x229c_c6dc, 0xa860, 0x4b52, 0xb4, 0xd8, 0x05, 0x3a, 0x22, 0xb3, 0x89, 0x2b);
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct Guid(u32, u16, u16, u8, u8, u8, u8, u8, u8, u8, u8);
@@ -172,6 +175,8 @@ impl Encode for Codec {
             CodecProperty::Ignore => GUID_IGNORE,
             #[cfg(feature = "qoi")]
             CodecProperty::Qoi => GUID_QOI,
+            #[cfg(feature = "qoiz")]
+            CodecProperty::QoiZ => GUID_QOIZ,
             _ => return Err(other_err!("invalid codec")),
         };
         guid.encode(dst)?;
@@ -211,6 +216,8 @@ impl Encode for Codec {
             }
             #[cfg(feature = "qoi")]
             CodecProperty::Qoi => dst.write_u16(0),
+            #[cfg(feature = "qoiz")]
+            CodecProperty::QoiZ => dst.write_u16(0),
             CodecProperty::Ignore => dst.write_u16(0),
             CodecProperty::None => dst.write_u16(0),
         };
@@ -236,6 +243,8 @@ impl Encode for Codec {
                 },
                 #[cfg(feature = "qoi")]
                 CodecProperty::Qoi => 0,
+                #[cfg(feature = "qoiz")]
+                CodecProperty::QoiZ => 0,
                 CodecProperty::Ignore => 0,
                 CodecProperty::None => 0,
             }
@@ -280,6 +289,13 @@ impl<'de> Decode<'de> for Codec {
                 }
                 CodecProperty::Qoi
             }
+            #[cfg(feature = "qoiz")]
+            GUID_QOIZ => {
+                if !property_buffer.is_empty() {
+                    return Err(invalid_field_err!("qoi property", "must be empty"));
+                }
+                CodecProperty::QoiZ
+            }
             _ => CodecProperty::None,
         };
 
@@ -301,6 +317,8 @@ pub enum CodecProperty {
     Ignore,
     #[cfg(feature = "qoi")]
     Qoi,
+    #[cfg(feature = "qoiz")]
+    QoiZ,
     None,
 }
 
@@ -639,6 +657,7 @@ pub struct CodecId(u8);
 pub const CODEC_ID_NONE: CodecId = CodecId(0);
 pub const CODEC_ID_REMOTEFX: CodecId = CodecId(3);
 pub const CODEC_ID_QOI: CodecId = CodecId(0x0A);
+pub const CODEC_ID_QOIZ: CodecId = CodecId(0x0B);
 
 impl Debug for CodecId {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -646,6 +665,7 @@ impl Debug for CodecId {
             0 => "None",
             3 => "RemoteFx",
             0x0A => "QOI",
+            0x0B => "QOIZ",
             _ => "unknown",
         };
         write!(f, "CodecId({name})")
@@ -658,6 +678,7 @@ impl CodecId {
             0 => Some(CODEC_ID_NONE),
             3 => Some(CODEC_ID_REMOTEFX),
             0x0A => Some(CODEC_ID_QOI),
+            0x0B => Some(CODEC_ID_QOIZ),
             _ => None,
         }
     }
@@ -700,6 +721,7 @@ fn parse_codecs_config<'a>(codecs: &'a [&'a str]) -> Result<HashMap<&'a str, boo
 ///
 /// * `remotefx` (on by default)
 /// * `qoi` (on by default, when feature "qoi")
+/// * `qoiz` (on by default, when feature "qoiz")
 ///
 /// # Returns
 ///
@@ -711,6 +733,7 @@ pub fn client_codecs_capabilities(config: &[&str]) -> Result<BitmapCodecs, Strin
 List of codecs:
 - `remotefx` (on by default)
 - `qoi` (on by default, when feature "qoi")
+- `qoiz` (on by default, when feature "qoiz")
 "#
         .to_owned());
     }
@@ -739,6 +762,14 @@ List of codecs:
         });
     }
 
+    #[cfg(feature = "qoiz")]
+    if config.remove("qoiz").unwrap_or(true) {
+        codecs.push(Codec {
+            id: CODEC_ID_QOIZ.0,
+            property: CodecProperty::QoiZ,
+        });
+    }
+
     let codec_names = config.keys().copied().collect::<Vec<_>>().join(", ");
     if !codec_names.is_empty() {
         return Err(format!("Unknown codecs: {codec_names}"));
@@ -760,6 +791,7 @@ List of codecs:
 ///
 /// * `remotefx` (on by default)
 /// * `qoi` (on by default, when feature "qoi")
+/// * `qoiz` (on by default, when feature "qoiz")
 ///
 /// # Returns
 ///
@@ -771,6 +803,7 @@ pub fn server_codecs_capabilities(config: &[&str]) -> Result<BitmapCodecs, Strin
 List of codecs:
 - `remotefx` (on by default)
 - `qoi` (on by default, when feature "qoi")
+- `qoiz` (on by default, when feature "qoiz")
 "#
         .to_owned());
     }
@@ -794,6 +827,14 @@ List of codecs:
         codecs.push(Codec {
             id: 0,
             property: CodecProperty::Qoi,
+        });
+    }
+
+    #[cfg(feature = "qoiz")]
+    if config.remove("qoiz").unwrap_or(true) {
+        codecs.push(Codec {
+            id: 0,
+            property: CodecProperty::QoiZ,
         });
     }
 

commit ba4ac919d2816beeaac81451e2f4717f50eed9b7

fix(server): keep looping on compress_stream2() > 0
Fixes: 87df67fdc76 ("feat: add QOIZ image codec")

Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

crates/ironrdp-server/src/encoder/mod.rs
@@ -504,19 +504,24 @@ impl BitmapUpdateHandler for QoizHandler {
         let qoi = qoi_encode(bitmap)?;
         let mut inb = zstd_safe::InBuffer::around(&qoi);
         let mut data = vec![0; qoi.len()];
-        let mut outb = zstd_safe::OutBuffer::around(data.as_mut_slice());
+        let mut outb;
+        let mut pos = 0;
 
         let mut zctxt = self.zctxt.lock().unwrap();
-        let res = zctxt
-            .compress_stream2(
-                &mut outb,
-                &mut inb,
-                zstd_safe::zstd_sys::ZSTD_EndDirective::ZSTD_e_flush,
-            )
-            .map_err(zstd_safe::get_error_name)
-            .unwrap();
-        if res != 0 {
-            return Err(anyhow!("Failed to zstd compress"));
+        loop {
+            outb = zstd_safe::OutBuffer::around_pos(data.as_mut_slice(), pos);
+            let res = zctxt
+                .compress_stream2(
+                    &mut outb,
+                    &mut inb,
+                    zstd_safe::zstd_sys::ZSTD_EndDirective::ZSTD_e_flush,
+                )
+                .map_err(|code| anyhow!("Failed to zstd compress: {}", zstd_safe::get_error_name(code)))?;
+            if res == 0 {
+                break;
+            }
+            pos = outb.pos();
+            data.resize(data.len() + res, 0);
         }
 
         set_surface(bitmap, self.codec_id, outb.as_slice())