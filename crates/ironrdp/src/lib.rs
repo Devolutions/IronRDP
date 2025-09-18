a479aa6279f11af1d483976c2526ae956783aa96
Alexandr Yusuk <aleksandr.yusuk@apriorit.com>
2025-09-18 15:04:39 +0300

    fix lint

--- crates/ironrdp/src/lib.rs
+++ crates/ironrdp/src/lib.rs
@@ -4,7 +4,7 @@
 
 #[cfg(test)]
 use {
-    anyhow as _, async_trait as _, image as _, ironrdp_blocking as _, ironrdp_cliprdr_native as _,
+    anyhow as _, async_trait as _, image as _, ironrdp_blocking as _, ironrdp_cliprdr_native as _, opus2 as _,
     pico_args as _, rand as _, sspi as _, tokio_rustls as _, tracing as _, tracing_subscriber as _, x509_cert as _,
 };
 
