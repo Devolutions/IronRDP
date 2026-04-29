1a09dbaca9dd5d35025ee50aaa645100222be189
rhammonds-teleport <ryan.hammonds@goteleport.com>
2026-04-29 07:25:37 -0400

    feat(rdpdr): Notify RdpdrBackend of 'User Logged On' Messages (#1211)

--- crates/ironrdp-rdpdr/src/backend/mod.rs
+++ crates/ironrdp-rdpdr/src/backend/mod.rs
@@ -6,6 +6,7 @@ use ironrdp_core::AsAny;
 use ironrdp_pdu::PduResult;
 use ironrdp_svc::SvcMessage;
 
+use crate::Rdpdr;
 use crate::pdu::efs::{DeviceControlRequest, ServerDeviceAnnounceResponse, ServerDriveIoRequest};
 use crate::pdu::esc::{ScardCall, ScardIoCtlCode};
 
@@ -14,4 +15,7 @@ pub trait RdpdrBackend: AsAny + fmt::Debug + Send {
     fn handle_server_device_announce_response(&mut self, pdu: ServerDeviceAnnounceResponse) -> PduResult<()>;
     fn handle_scard_call(&mut self, req: DeviceControlRequest<ScardIoCtlCode>, call: ScardCall) -> PduResult<()>;
     fn handle_drive_io_request(&mut self, req: ServerDriveIoRequest) -> PduResult<Vec<SvcMessage>>;
+    fn handle_user_logged_on(&mut self, _rdpdr: &mut Rdpdr) -> PduResult<Vec<SvcMessage>> {
+        Ok(Vec::new())
+    }
 }
