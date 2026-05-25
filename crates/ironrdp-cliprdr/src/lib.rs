a4bc4753607d87ef0989d9df16a31cd22e7c7fde
clintcan <clintcan@users.noreply.github.com>
2026-05-25 21:59:18 +0800

    feat(cliprdr): add CliprdrBackend::on_format_list_response(ok) hook (#1300)

--- crates/ironrdp-cliprdr/src/lib.rs
+++ crates/ironrdp-cliprdr/src/lib.rs
@@ -627,6 +627,7 @@ impl<R: Role> Cliprdr<R> {
                         info!("Remote accepted format list");
                     }
                 }
+                self.backend.on_format_list_response(true);
             }
             FormatListResponse::Fail => {
                 // [MS-RDPECLIP] 3.1.5.2.4 - The remote rejected our FormatList but the
@@ -654,6 +655,7 @@ impl<R: Role> Cliprdr<R> {
 
                     self.sent_file_contents_requests.clear();
                 }
+                self.backend.on_format_list_response(false);
             }
         }
 
