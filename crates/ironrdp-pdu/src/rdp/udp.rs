720d881caeb6c48f003d99e3840ebc9f85b9a08c
Marc-André Moreau <mamoreau@devolutions.net>
2026-08-10 16:55:49 -0400

    feat(pdu): encode optional RDP-UDP headers
    Add the FEC, payload-prefix, ACK-of-ACK, correlation, and extended SYN codecs. Keep source-payload parsing header-only so callers retain the following payload bytes.

--- crates/ironrdp-pdu/src/rdp/udp.rs
+++ crates/ironrdp-pdu/src/rdp/udp.rs
@@ -1,12 +1,12 @@
 //! RDP-UDP reliable transport wire structures.
 //!
 //! The transport state machine lives outside this crate. These types encode the
-//! common header, connection-initialization, acknowledgement, and source-data
-//! structures used by reliable RDP-UDP versions 1 and 2.
+//! common header, connection-initialization, acknowledgement, and data payload
+//! headers used by reliable RDP-UDP.
 //!
-//! Defined in [\[MS-RDPEUDP\] 2.2.2.1, 2.2.2.4, 2.2.2.5, and 2.2.2.7].
+//! Defined in [\[MS-RDPEUDP\] 2.2.2.1 through 2.2.2.9].
 //!
-//! [\[MS-RDPEUDP\] 2.2.2.1, 2.2.2.4, 2.2.2.5, and 2.2.2.7]: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-rdpeudp/2744a3ee-04fb-407b-a9e3-b3b2ded422b1
+//! [\[MS-RDPEUDP\] 2.2.2.1 through 2.2.2.9]: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-rdpeudp/2744a3ee-04fb-407b-a9e3-b3b2ded422b1
 
 use bitflags::bitflags;
 use ironrdp_core::{
@@ -38,7 +38,7 @@ bitflags! {
         const ACK = 0x0004;
         /// A data payload follows the ACK structures.
         const DATA = 0x0008;
-        /// The data payload is FEC encoded and follows an FEC payload header.
+        /// An FEC payload header follows the common header.
         const FEC = 0x0010;
         /// Congestion notification.
         const CN = 0x0020;
@@ -46,13 +46,13 @@ bitflags! {
         const CWR = 0x0040;
         /// Selective ACK option; currently unused by the protocol.
         const SACK_OPTION = 0x0080;
-        /// ACK-of-ACK vector follows the ACK vector.
+        /// An ACK-of-ACK vector header follows the ACK vector.
         const ACK_OF_ACKS = 0x0100;
-        /// Requests lossy transport; this is unsupported by the reliable-only engine.
+        /// Requests lossy transport.
         const SYN_LOSSY = 0x0200;
         /// Indicates delayed acknowledgement.
         const ACK_DELAYED = 0x0400;
-        /// A correlation ID follows SYN data.
+        /// A correlation ID payload follows SYN data.
         const CORRELATION_ID = 0x0800;
         /// Extended SYN data follows SYN data.
         const SYN_EX = 0x1000;
@@ -111,6 +111,110 @@ impl<'de> Decode<'de> for RdpUdpFecHeader {
             source_ack,
             receive_window_size,
             flags,
+        })
+    }
+}
+
+/// FEC payload metadata.
+///
+/// Defined in [\[MS-RDPEUDP\] 2.2.2.2].
+///
+/// [\[MS-RDPEUDP\] 2.2.2.2]: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-rdpeudp/2744a3ee-04fb-407b-a9e3-b3b2ded422b1
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[cfg_attr(feature = "arbitrary", derive(arbitrary::Arbitrary))]
+pub struct RdpUdpFecPayloadHeader {
+    /// Sequence number of this coded packet.
+    pub coded_sequence_number: u32,
+    /// First source sequence number contained in the FEC payload.
+    pub source_sequence_number: u32,
+    /// Source-packet range covered by the FEC payload.
+    pub range: u8,
+    /// FEC engine index.
+    pub fec_index: u8,
+    /// Padding bytes supplied by the FEC engine.
+    pub padding: [u8; 2],
+}
+
+impl RdpUdpFecPayloadHeader {
+    const FIXED_PART_SIZE: usize = 4 /* snCoded */
+        + 4 /* snSourceStart */
+        + 1 /* uRange */
+        + 1 /* uFecIndex */
+        + 2 /* uPadding */;
+    const NAME: &'static str = "RdpUdpFecPayloadHeader";
+}
+
+impl Encode for RdpUdpFecPayloadHeader {
+    fn encode(&self, dst: &mut WriteCursor<'_>) -> EncodeResult<()> {
+        ensure_fixed_part_size!(in: dst);
+        dst.write_u32_be(self.coded_sequence_number);
+        dst.write_u32_be(self.source_sequence_number);
+        dst.write_u8(self.range);
+        dst.write_u8(self.fec_index);
+        dst.write_array(self.padding);
+        Ok(())
+    }
+
+    fn name(&self) -> &'static str {
+        Self::NAME
+    }
+
+    fn size(&self) -> usize {
+        Self::FIXED_PART_SIZE
+    }
+}
+
+impl<'de> Decode<'de> for RdpUdpFecPayloadHeader {
+    fn decode(src: &mut ReadCursor<'de>) -> DecodeResult<Self> {
+        ensure_fixed_part_size!(in: src);
+        Ok(Self {
+            coded_sequence_number: src.read_u32_be(),
+            source_sequence_number: src.read_u32_be(),
+            range: src.read_u8(),
+            fec_index: src.read_u8(),
+            padding: src.read_array(),
+        })
+    }
+}
+
+/// Length prefix for a recovered FEC payload.
+///
+/// Defined in [\[MS-RDPEUDP\] 2.2.2.3].
+///
+/// [\[MS-RDPEUDP\] 2.2.2.3]: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-rdpeudp/2744a3ee-04fb-407b-a9e3-b3b2ded422b1
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[cfg_attr(feature = "arbitrary", derive(arbitrary::Arbitrary))]
+pub struct RdpUdpPayloadPrefix {
+    /// Length of the recovered payload.
+    pub payload_size: u16,
+}
+
+impl RdpUdpPayloadPrefix {
+    const FIXED_PART_SIZE: usize = 2 /* cbPayloadSize */;
+    const NAME: &'static str = "RdpUdpPayloadPrefix";
+}
+
+impl Encode for RdpUdpPayloadPrefix {
+    fn encode(&self, dst: &mut WriteCursor<'_>) -> EncodeResult<()> {
+        ensure_fixed_part_size!(in: dst);
+        dst.write_u16_be(self.payload_size);
+        Ok(())
+    }
+
+    fn name(&self) -> &'static str {
+        Self::NAME
+    }
+
+    fn size(&self) -> usize {
+        Self::FIXED_PART_SIZE
+    }
+}
+
+impl<'de> Decode<'de> for RdpUdpPayloadPrefix {
+    fn decode(src: &mut ReadCursor<'de>) -> DecodeResult<Self> {
+        ensure_fixed_part_size!(in: src);
+        Ok(Self {
+            payload_size: src.read_u16_be(),
         })
     }
 }
@@ -197,6 +301,48 @@ impl<'de> Decode<'de> for RdpUdpSynData {
     }
 }
 
+/// ACK-vector reset metadata.
+///
+/// Defined in [\[MS-RDPEUDP\] 2.2.2.6].
+///
+/// [\[MS-RDPEUDP\] 2.2.2.6]: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-rdpeudp/2744a3ee-04fb-407b-a9e3-b3b2ded422b1
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[cfg_attr(feature = "arbitrary", derive(arbitrary::Arbitrary))]
+pub struct RdpUdpAckOfAckVectorHeader {
+    /// Sequence number that resets ACK-vector encoding state.
+    pub reset_sequence_number: u32,
+}
+
+impl RdpUdpAckOfAckVectorHeader {
+    const FIXED_PART_SIZE: usize = 4 /* snResetSeqNum */;
+    const NAME: &'static str = "RdpUdpAckOfAckVectorHeader";
+}
+
+impl Encode for RdpUdpAckOfAckVectorHeader {
+    fn encode(&self, dst: &mut WriteCursor<'_>) -> EncodeResult<()> {
+        ensure_fixed_part_size!(in: dst);
+        dst.write_u32_be(self.reset_sequence_number);
+        Ok(())
+    }
+
+    fn name(&self) -> &'static str {
+        Self::NAME
+    }
+
+    fn size(&self) -> usize {
+        Self::FIXED_PART_SIZE
+    }
+}
+
+impl<'de> Decode<'de> for RdpUdpAckOfAckVectorHeader {
+    fn decode(src: &mut ReadCursor<'de>) -> DecodeResult<Self> {
+        ensure_fixed_part_size!(in: src);
+        Ok(Self {
+            reset_sequence_number: src.read_u32_be(),
+        })
+    }
+}
+
 /// RDP-UDP ACK vector with DWORD alignment padding.
 ///
 /// Defined in [\[MS-RDPEUDP\] 2.2.2.7].
@@ -258,7 +404,7 @@ impl<'de> Decode<'de> for RdpUdpAckVector {
     }
 }
 
-/// Header and bytes of a reliable RDP-UDP source payload.
+/// Header of a reliable RDP-UDP source payload.
 ///
 /// Defined in [\[MS-RDPEUDP\] 2.2.2.4].
 ///
@@ -270,8 +416,6 @@ pub struct RdpUdpSourcePayload {
     pub coded_sequence_number: u32,
     /// Sequence number of the encapsulated source payload.
     pub source_sequence_number: u32,
-    /// Bytes delivered to the upper transport layer.
-    pub data: Vec<u8>,
 }
 
 impl RdpUdpSourcePayload {
@@ -281,10 +425,9 @@ impl RdpUdpSourcePayload {
 
 impl Encode for RdpUdpSourcePayload {
     fn encode(&self, dst: &mut WriteCursor<'_>) -> EncodeResult<()> {
-        ensure_size!(in: dst, size: self.size());
+        ensure_fixed_part_size!(in: dst);
         dst.write_u32_be(self.coded_sequence_number);
         dst.write_u32_be(self.source_sequence_number);
-        dst.write_slice(&self.data);
         Ok(())
     }
 
@@ -293,7 +436,7 @@ impl Encode for RdpUdpSourcePayload {
     }
 
     fn size(&self) -> usize {
-        Self::FIXED_PART_SIZE + self.data.len()
+        Self::FIXED_PART_SIZE
     }
 }
 
@@ -302,12 +445,148 @@ impl<'de> Decode<'de> for RdpUdpSourcePayload {
         ensure_fixed_part_size!(in: src);
         let coded_sequence_number = src.read_u32_be();
         let source_sequence_number = src.read_u32_be();
-        let data = src.read_remaining().to_vec();
 
         Ok(Self {
             coded_sequence_number,
             source_sequence_number,
-            data,
+        })
+    }
+}
+
+/// Correlation ID negotiated by the RDP-UDP endpoint.
+///
+/// Defined in [\[MS-RDPEUDP\] 2.2.2.8].
+///
+/// [\[MS-RDPEUDP\] 2.2.2.8]: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-rdpeudp/2744a3ee-04fb-407b-a9e3-b3b2ded422b1
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[cfg_attr(feature = "arbitrary", derive(arbitrary::Arbitrary))]
+pub struct RdpUdpCorrelationId {
+    /// Connection correlation identifier in wire byte order.
+    pub correlation_id: [u8; 16],
+}
+
+impl RdpUdpCorrelationId {
+    const FIXED_PART_SIZE: usize = 16 /* uCorrelationId */ + 16 /* uReserved */;
+    const NAME: &'static str = "RdpUdpCorrelationId";
+}
+
+impl Encode for RdpUdpCorrelationId {
+    fn encode(&self, dst: &mut WriteCursor<'_>) -> EncodeResult<()> {
+        ensure_fixed_part_size!(in: dst);
+        dst.write_array(self.correlation_id);
+        dst.write_array([0; 16]);
+        Ok(())
+    }
+
+    fn name(&self) -> &'static str {
+        Self::NAME
+    }
+
+    fn size(&self) -> usize {
+        Self::FIXED_PART_SIZE
+    }
+}
+
+impl<'de> Decode<'de> for RdpUdpCorrelationId {
+    fn decode(src: &mut ReadCursor<'de>) -> DecodeResult<Self> {
+        ensure_fixed_part_size!(in: src);
+        let correlation_id = src.read_array();
+        let reserved = src.read_array::<16>();
+        if reserved != [0; 16] {
+            return Err(invalid_field_err!("uReserved", "must be all zeroes"));
+        }
+
+        Ok(Self { correlation_id })
+    }
+}
+
+bitflags! {
+    /// Extended SYN-data flags.
+    ///
+    /// Defined in [\[MS-RDPEUDP\] 2.2.2.9].
+    ///
+    /// [\[MS-RDPEUDP\] 2.2.2.9]: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-rdpeudp/2744a3ee-04fb-407b-a9e3-b3b2ded422b1
+    #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+    #[cfg_attr(feature = "arbitrary", derive(arbitrary::Arbitrary))]
+    pub struct RdpUdpSynDataExFlags: u16 {
+        /// The protocol version field is valid.
+        const VERSION_INFO_VALID = 0x0001;
+    }
+}
+
+/// RDP-UDP protocol version 3.
+///
+/// Version 3 uses the data transfer messages defined by MS-RDPEUDP2.
+pub const RDPUDP_PROTOCOL_VERSION_3: u16 = 0x0101;
+
+/// Extended RDP-UDP SYN parameters.
+///
+/// Defined in [\[MS-RDPEUDP\] 2.2.2.9].
+///
+/// [\[MS-RDPEUDP\] 2.2.2.9]: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-rdpeudp/2744a3ee-04fb-407b-a9e3-b3b2ded422b1
+#[derive(Debug, Clone, PartialEq, Eq)]
+#[cfg_attr(feature = "arbitrary", derive(arbitrary::Arbitrary))]
+pub struct RdpUdpSynDataEx {
+    /// Extended SYN options.
+    pub flags: RdpUdpSynDataExFlags,
+    /// Highest supported RDP-UDP protocol version.
+    pub protocol_version: u16,
+    /// Required cookie hash for protocol version 3, absent for all other versions.
+    pub cookie_hash: Option<[u8; 32]>,
+}
+
+impl RdpUdpSynDataEx {
+    const FIXED_PART_SIZE: usize = 2 /* uSynExFlags */ + 2 /* uUdpVer */;
+    const NAME: &'static str = "RdpUdpSynDataEx";
+
+    fn has_valid_cookie_hash(&self) -> bool {
+        (self.protocol_version == RDPUDP_PROTOCOL_VERSION_3) == self.cookie_hash.is_some()
+    }
+}
+
+impl Encode for RdpUdpSynDataEx {
+    fn encode(&self, dst: &mut WriteCursor<'_>) -> EncodeResult<()> {
+        if !self.has_valid_cookie_hash() {
+            return Err(invalid_field_err!(
+                "cookieHash",
+                "must be present only for RDPUDP_PROTOCOL_VERSION_3"
+            ));
+        }
+
+        ensure_size!(in: dst, size: self.size());
+        dst.write_u16_be(self.flags.bits());
+        dst.write_u16_be(self.protocol_version);
+        if let Some(cookie_hash) = self.cookie_hash {
+            dst.write_array(cookie_hash);
+        }
+        Ok(())
+    }
+
+    fn name(&self) -> &'static str {
+        Self::NAME
+    }
+
+    fn size(&self) -> usize {
+        Self::FIXED_PART_SIZE + self.cookie_hash.map_or(0, |hash| hash.len())
+    }
+}
+
+impl<'de> Decode<'de> for RdpUdpSynDataEx {
+    fn decode(src: &mut ReadCursor<'de>) -> DecodeResult<Self> {
+        ensure_fixed_part_size!(in: src);
+        let flags = RdpUdpSynDataExFlags::from_bits_truncate(src.read_u16_be());
+        let protocol_version = src.read_u16_be();
+        let cookie_hash = if protocol_version == RDPUDP_PROTOCOL_VERSION_3 {
+            ensure_size!(in: src, size: 32);
+            Some(src.read_array())
+        } else {
+            None
+        };
+
+        Ok(Self {
+            flags,
+            protocol_version,
+            cookie_hash,
         })
     }
 }
@@ -383,5 +662,86 @@ mod tests {
 
         let invalid_ack_size = [0x08, 0x01];
         assert!(ironrdp_core::decode::<RdpUdpAckVector>(&invalid_ack_size).is_err());
+    }
+
+    #[test]
+    fn supports_optional_udp_header_codecs() {
+        let fec = RdpUdpFecPayloadHeader {
+            coded_sequence_number: 1,
+            source_sequence_number: 2,
+            range: 3,
+            fec_index: 4,
+            padding: [5, 6],
+        };
+        let prefix = RdpUdpPayloadPrefix { payload_size: 1232 };
+        let ack_of_acks = RdpUdpAckOfAckVectorHeader {
+            reset_sequence_number: 7,
+        };
+        let correlation = RdpUdpCorrelationId {
+            correlation_id: [8; 16],
+        };
+        let syn_ex_v2 = RdpUdpSynDataEx {
+            flags: RdpUdpSynDataExFlags::VERSION_INFO_VALID,
+            protocol_version: 2,
+            cookie_hash: None,
+        };
+        let syn_ex_v3 = RdpUdpSynDataEx {
+            flags: RdpUdpSynDataExFlags::VERSION_INFO_VALID,
+            protocol_version: RDPUDP_PROTOCOL_VERSION_3,
+            cookie_hash: Some([9; 32]),
+        };
+
+        let encoded = ironrdp_core::encode_vec(&fec).unwrap();
+        assert_eq!(ironrdp_core::decode::<RdpUdpFecPayloadHeader>(&encoded).unwrap(), fec);
+        let encoded = ironrdp_core::encode_vec(&prefix).unwrap();
+        assert_eq!(ironrdp_core::decode::<RdpUdpPayloadPrefix>(&encoded).unwrap(), prefix);
+        let encoded = ironrdp_core::encode_vec(&ack_of_acks).unwrap();
+        assert_eq!(
+            ironrdp_core::decode::<RdpUdpAckOfAckVectorHeader>(&encoded).unwrap(),
+            ack_of_acks
+        );
+        let encoded = ironrdp_core::encode_vec(&correlation).unwrap();
+        assert_eq!(
+            ironrdp_core::decode::<RdpUdpCorrelationId>(&encoded).unwrap(),
+            correlation
+        );
+        let encoded = ironrdp_core::encode_vec(&syn_ex_v2).unwrap();
+        assert_eq!(ironrdp_core::decode::<RdpUdpSynDataEx>(&encoded).unwrap(), syn_ex_v2);
+        let encoded = ironrdp_core::encode_vec(&syn_ex_v3).unwrap();
+        assert_eq!(ironrdp_core::decode::<RdpUdpSynDataEx>(&encoded).unwrap(), syn_ex_v3);
+    }
+
+    #[test]
+    fn source_payload_header_preserves_following_payload() {
+        let encoded = [
+            0, 0, 0, 1, // snCoded
+            0, 0, 0, 2, // snSourceStart
+            3, 4, 5, // source payload
+        ];
+        let mut cursor = ReadCursor::new(&encoded);
+        assert_eq!(
+            RdpUdpSourcePayload::decode(&mut cursor).unwrap(),
+            RdpUdpSourcePayload {
+                coded_sequence_number: 1,
+                source_sequence_number: 2,
+            }
+        );
+        assert_eq!(cursor.remaining(), &[3, 4, 5]);
+    }
+
+    #[test]
+    fn rejects_invalid_optional_udp_header_fields() {
+        let correlation = [0; 32];
+        let mut invalid_correlation = correlation;
+        invalid_correlation[16] = 1;
+        assert!(ironrdp_core::decode::<RdpUdpCorrelationId>(&invalid_correlation).is_err());
+
+        let syn_ex = RdpUdpSynDataEx {
+            flags: RdpUdpSynDataExFlags::VERSION_INFO_VALID,
+            protocol_version: RDPUDP_PROTOCOL_VERSION_3,
+            cookie_hash: None,
+        };
+        assert!(ironrdp_core::encode_vec(&syn_ex).is_err());
+        assert!(ironrdp_core::decode::<RdpUdpSynDataEx>(&[0, 1, 1, 1]).is_err());
     }
 }
